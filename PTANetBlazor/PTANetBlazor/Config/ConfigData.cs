9468a75f8b2642c31e2b3046f26c209f53492677
JuanFrancisco <juanfranciscofoncubierta@gmail.com>
2024-05-15 19:09:52 +0200

    Added config file

---
+++ PTANetBlazor/PTANetBlazor/Config/ConfigData.cs
@@ -0,0 +1,14 @@
+﻿namespace PTANetBlazor;
+
+public class ConfigData
+{
+    public static ConfigData Instance = new("");
+
+
+    public string urlApi = "https://api.opendata.esett.com/EXP01/BalanceResponsibleParties";
+
+    public ConfigData(string _urlApi) { 
+        urlApi = _urlApi;
+    }
+}
+
