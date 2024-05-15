dcf6ff4bb566c5348456d010258c844d73616849
JuanFrancisco <juanfranciscofoncubierta@gmail.com>
2024-05-15 09:28:49 +0200

    Add database with entity framework

---
+++ PTANetBlazor/PTANetBlazor/Database/BalanceResponsiblepartiesDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PTAJuan.Model;
+namespace PTAJuan.Database;
+
+public class BalanceResponsibleContext : DbContext
+{
+    public BalanceResponsibleContext() : base()
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer();
+    }
+    public DbSet<BalanceResponsibleParty> balanceResponsibleParties {  get; set; }
+
+}
+
